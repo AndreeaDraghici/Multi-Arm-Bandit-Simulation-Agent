a8220e2f50f70eb88b0cb1e42ca31d2695a0ed61
AndreeaDraghici <draghici108@gmail.com>
2023-11-25 19:28:21 +0200

    extend UI functionality

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ from src.model.gui.banditsimulationgui import BanditSimulationGUI
 if __name__ == '__main__':
     root = Tk()
     app = BanditSimulationGUI(root)
-    root.geometry("600x500")  # Set the initial size of the window
+    root.geometry("670x600")  # Set the initial size of the window
     root.mainloop()
--- src/model/gui/banditsimulationgui.py
+++ src/model/gui/banditsimulationgui.py
@@ -9,44 +9,73 @@ from src.model.multiarmedbandit import MultiArmedBandit
 
 class BanditSimulationGUI :
     def __init__(self, root) :
+        """
+        Initializes the BanditSimulationGUI.
+        :param root: The Tkinter root window.
+        """
         self.root = root
         self.root.title("Multi Arm Bandit Simulation")
 
-        self.label_path = Label(root, text="Enter path to input file:")
-        self.label_path.pack(pady=5)
+        # Configure column and row weights for resizing
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        self.root.rowconfigure(0, weight=0)
+        self.root.rowconfigure(1, weight=0)
+        self.root.rowconfigure(2, weight=1)
 
-        self.entry_path = Entry(root)
-        self.entry_path.pack(pady=5)
+        # Label, Entry, and Button for the file path
+        self.label_path = Label(root, text="Enter path to input file")
+        self.label_path.grid(row=0, column=0, pady=5, sticky='e')
+
+        self.entry_path = Entry(root, width=40)
+        self.entry_path.grid(row=0, column=1, pady=5, sticky='we')
 
         self.button_browse = Button(root, text="Browse", command=self.browse_file)
-        self.button_browse.pack(pady=5)
+        self.button_browse.grid(row=0, column=2, pady=5, sticky='w')
 
+        # Button to run the simulation
         self.button_run_simulation = Button(root, text="Run Simulation", command=self.run_simulation)
-        self.button_run_simulation.pack(pady=10)
+        self.button_run_simulation.grid(row=1, column=1, pady=10)
 
+        # Figure and canvas for the plot
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=root)
         self.canvas_widget = self.canvas.get_tk_widget()
-        self.canvas_widget.pack(expand=True, fill="both", padx=10, pady=10)
+        self.canvas_widget.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky='nsew')
 
     def browse_file(self) :
+        """
+        Opens a file dialog to select the input file and updates the entry field with the selected file path.
+        """
+        # Open a file dialog to select the input file
         file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+
+        # Update the entry field with the selected file path
         self.entry_path.delete(0, 'end')
         self.entry_path.insert(0, file_path)
 
     def run_simulation(self) :
+        """
+        Runs the bandit simulation based on the provided input file.
+
+        Reads the number of arms, total iterations, and epsilon from the input file,
+        creates a multi-armed bandit and agents, simulates iterations, and plots the results.
+        """
+
+        # Get the file path from the entry field
         file_path = self.entry_path.get()
 
-        # Reading the number of arms, total iterations, and epsilon from the input file
+        # Read the number of arms, total iterations, and epsilon from the input file
         with open(file_path, 'r') as file :
             no_arms = int(file.readline().strip())
             num_iterations = int(file.readline().strip())
             epsilon = float(file.readline().strip())
 
-        # Creating the multi-armed bandit
+        # Create the multi-armed bandit
         bandit = MultiArmedBandit(no_arms)
 
-        # Creating the agents
+        # Create the agents
         ucb1_agent = UCB1Agent(no_arms)
         epsilon_greedy_agent = EpsilonGreedyAgent(no_arms, eps=epsilon)
 
@@ -54,26 +83,26 @@ class BanditSimulationGUI :
         avg_rewards_ucb1 = []
         avg_rewards_epsilon_greedy = []
 
-        # Simulating iterations
+        # Simulate iterations
         for i in range(num_iterations) :
-            # Selecting arms for each agent
+            # Select arms for each agent
             arm_ucb1 = ucb1_agent.select_arm()
             arm_epsilon_greedy = epsilon_greedy_agent.select_arm()
 
-            # Simulating pulling arms and obtaining rewards
+            # Simulate pulling arms and obtaining rewards
             reward_ucb1 = bandit.pull_arm(arm_ucb1)
             reward_epsilon_greedy = bandit.pull_arm(arm_epsilon_greedy)
 
-            # Updating agents with obtained rewards
+            # Update agents with obtained rewards
             ucb1_agent.update(arm_ucb1, reward_ucb1)
             epsilon_greedy_agent.update(arm_epsilon_greedy, reward_epsilon_greedy)
 
-            # Calculating average rewards and adding them to the list
+            # Calculate average rewards and add them to the list
             avg_rewards_ucb1.append(np.mean(ucb1_agent.total_rewards / (ucb1_agent.num_pulls + 1e-6)))
             avg_rewards_epsilon_greedy.append(
                 np.mean(epsilon_greedy_agent.total_rewards / (epsilon_greedy_agent.num_pulls + 1e-6)))
 
-        # Plotting the results
+        # Plot the results
         self.ax.clear()
         self.ax.plot(avg_rewards_ucb1, label='UCB1')
         self.ax.plot(avg_rewards_epsilon_greedy, label='Epsilon-Greedy')
@@ -81,5 +110,5 @@ class BanditSimulationGUI :
         self.ax.set_ylabel('Average Reward')
         self.ax.legend()
 
-        # Refreshing the canvas
+        # Refresh the canvas
         self.canvas.draw()
